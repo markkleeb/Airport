92016f169ca2788fd18709a7f5920c8ebe632599
markkleeb <kleeb930@gmail.com>
2012-02-24 19:24:11 -0500

    first commit

---
+++ src/testApp.cpp
@@ -0,0 +1,103 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofBackground(0, 255, 0 );
+    
+    ofSetFrameRate(36);
+	
+	framenum=0;
+	doCapture=false;
+	
+	for(int i=0; i<10; i++) {
+		Boid b;
+		boids.push_back( b );
+	}
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+    for(int i=0; i<boids.size(); i++)
+	{
+		if(i<5) {
+			boids[i].seek( ofPoint(mouseX, mouseY) );
+		} else {
+			boids[i].arrive( ofPoint(mouseX, mouseY) );
+		}
+        
+		boids[i].update();
+	}
+    
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    ofNoFill();
+	ofCircle(mouseX, mouseY, 20);
+	
+	for(int i=0; i<boids.size(); i++) {
+		boids[i].draw();
+	}
+	capture();
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+   if(key=='c') doCapture = !doCapture;
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+void testApp::capture() {
+	if(doCapture && ofGetFrameNum() % 4 == 0)
+	{
+		char filename[255];
+		sprintf(filename, "frame%05d.png", framenum++);
+		ofSaveScreen(filename);
+	}
+}
+
+
